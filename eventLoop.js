cd5a58b19c0951491b9fd3c1b83b997b75bea353
KonthamDivyateja <139646440+KonthamDivyateja@users.noreply.github.com>
2025-11-27 21:04:40 +0530

    Add files via upload

---
+++ eventLoop.js
@@ -0,0 +1,11 @@
+console.log("Begin");
+
+Promise.resolve().then(() => {
+    console.log("Promise Task");
+});
+
+setTimeout(() => {
+    console.log("Timeout Task");
+}, 0);
+
+console.log("End");
